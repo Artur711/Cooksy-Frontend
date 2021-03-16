d8a2eae83c6cc851febd2a9111a00e81c414ff68
Artur <arturkubiak9@gmail.com>
2021-03-16 18:56:50 +0100

    Test2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+    apiUrlHost: 'https://cooksy-api.herokuapp.com',
+    apiUrl: 'https://cooksy-api.herokuapp.com/api/v1',
+    apiRecipeUrl: 'https://cooksy-api.herokuapp.com',
 };
